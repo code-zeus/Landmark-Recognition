99ad59969b7d4b4a5d1fc3074662e4ce867328b0
code-zeus <dixitkartikey08@gmail.com>
2019-03-10 01:45:31 +0530

    9 bin histogram generated

--- landmark.py
+++ landmark.py
@@ -12,44 +12,46 @@ import urllib.request as ur
 import cv2
 
 def conv(mag,angle,b,a) :
-	mat=np.zeros([10])
+	mat=np.zeros(9)
 	for i in range(17*a,17*a+16):
 		for j in range(17*b,17*b+16):
-			if(angle[i][j]>180):
-				angle[i][j]=angle[i][j]%180
-				x=angle[i][j]%20
-				z=angle[i][j]/20
-				y=int(z)
-				#print(type(mag[i][j]))
-				mat[y]+=mag[i][j]*((20-x)/20)
-				if(x>8):
-					mat[0]+=mag[i][j]-mat[y-1]
-				else:
-					mat[y+1]+=mag[i][j]-mat[y-1]
+			m=0
+			if(angle[i][j]>=180):
+				m=angle[i][j]%180
+			x=m%20
+			y=m/20
+			z=int(y)
+			p=mag[i][j]*((20-x)/20)
+			mat[z]+=p
+			if(y>8):
+				mat[1]+=mag[i][j]-p
+			else:
+				mat[z+1]+=mag[i][j]-p
 	return mat
 
 
 df=pd.read_csv("/home/kartikey/Desktop/Landmark/landmarks.csv")
 df1=df["URL"]
-for i in range(89):
+for i in range(1):
 	file_name='image.jpg'
-	ur.urlretrieve(df1[i],file_name)
+	ur.urlretrieve(df1[0],file_name)
 	img = cv2.imread(file_name,0)
 	newimg= cv2.resize(img,(102,255))
 	cv2.imshow('image',newimg)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
-	print(newimg)
+	#print(newimg)
 	newimg = np.float32(newimg) / 255.0 
 	gx = cv2.Sobel(newimg, cv2.CV_32F, 1, 0, ksize=1)
 	gy = cv2.Sobel(newimg, cv2.CV_32F, 0, 1, ksize=1)
 	mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
-	temp=0
-	bin_hist=np.zeros([91,10])
+	for m in range(0,16):
+		for n in range(0,16):
+			print(mag[m][n], end = ' ' )
+		print('\n')
+	bin_hist=np.zeros((15,6,9))
 	for k in range(0,14):
 		for j in range(0,5):
-			bin_hist[temp]=conv(mag,angle,j,k)
-			temp+=1
-	cv2.imshow('image',angle)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+			bin_hist[k][j]=conv(mag,angle,j,i)
+	
+
