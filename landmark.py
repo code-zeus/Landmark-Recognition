d679c9cdfc79a73a0c4903ea81d5edfb6dcf64c9
Adityash786 <shuklaaditya98@gmail.com>
2019-03-11 22:02:24 +0530

    update

--- landmark.py
+++ landmark.py
@@ -3,10 +3,10 @@
 import numpy as np
 from normailze import normalize
 import pandas as pd
-import urllib.request as ur
+import requests
 import cv2
 from sklearn import svm
-import image-retreival
+from sklearn.metrics import classification_report,accuracy_score
 
 def conv(mag,angle,b,a) :
 	mat=np.zeros(9)
@@ -27,39 +27,49 @@ def conv(mag,angle,b,a) :
 	return mat
               
 
-df=pd.read_csv(r"C:\Users\pushkarpathak\Desktop\Landmark-Recognition\landmarks.csv")
+df=pd.read_csv(r"C:\Users\Aditya\Desktop\Landmark-Recognition\landmarks.csv")
 df1=df["URL"]
-for i in range(88):
+hog_features = []
+for i in range(0,81):
 	file_name='image.jpg'
-    url = df1[i]
-    r = requests.get(url, allow_redirects=True)
-    open(file_name, 'wb').write(r.content)
-	img = cv2.imread(file_name,0)
-	newimg= cv2.resize(img,(102,255))
-	cv2.imshow('image',newimg)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	url = df1[i]
+	r = requests.get(url, allow_redirects=True)
+	try:
+		open(file_name, 'wb').write(r.content)
+		img = cv2.imread(file_name,0)
+		newimg= cv2.resize(img,(102,255))
+	except Exception as e:
+		print(str(e))
+		continue
+	#cv2.imshow('image',newimg)
+	#cv2.waitKey(0)
+	#cv2.destroyAllWindows()
+	print(i)
 	newimg = np.float32(newimg) / 255.0 
 	gx = cv2.Sobel(newimg, cv2.CV_32F, 1, 0, ksize=1)
 	gy = cv2.Sobel(newimg, cv2.CV_32F, 0, 1, ksize=1)
 	mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+	if(i==15):
+		print(len(angle),len(angle[0]))
 	bin_hist=np.zeros((15,6,9))
 	for k in range(0,15):
 		for j in range(0,6):
-			bin_hist[k][j]=conv(mag,angle,j,i)   
-            
-            
-            
-hog_descriptor=np.zeros((0))
-for i in range(0,14):
-    for j in range(0,5):
-        temp=np.concatenate((bin_hist[i][j],bin_hist[i+1][j],bin_hist[i][j+1],bin_hist[i+1][j+1]))
-        hog_descriptor=np.concatenate((hog_descriptor,normalize(temp)))          
+			bin_hist[k][j]=conv(mag,angle,j,k)
 
 
-labels =  np.array(df['labels']).reshape(len(df['labels']),1)  
+	hog_descriptor=np.zeros((0))
+	for k in range(0,14):
+		for j in range(0,5):
+			temp=np.concatenate((bin_hist[k][j],bin_hist[k+1][j],bin_hist[k][j+1],bin_hist[k+1][j+1]))
+			hog_descriptor=np.concatenate((hog_descriptor,normalize(temp)))
+	hog_features.append(hog_descriptor)
+
+
+labels =  np.array(df['labels']).reshape(len(df['labels']),1) 
 clf = svm.SVC()
-hog_features = np.array(hog_descriptor)
+hog_features = np.array(hog_features)
+print(len(hog_features), len(hog_features[0]))
+print(len(labels), len(labels[0]))
 data_frame = np.hstack((hog_features,labels))
 np.random.shuffle(data_frame)
 
@@ -74,3 +84,6 @@ clf.fit(x_train,y_train)
 
 y_pred = clf.predict(x_test)
 
+print("Accuracy: "+str(accuracy_score(y_test, y_pred)))
+print('\n')
+print(classification_report(y_test, y_pred))
